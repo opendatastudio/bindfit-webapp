79ed2850ed54434556405961002d4180bb1907bd
Varvara Efremova <varvara@echus.co>
2015-09-03 19:08:45 +1000

    Added S3 URL to fingerprints

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    fingerprint: {
+      prepend: 'http://supramolecular.org.s3.amazonaws.com/'
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
