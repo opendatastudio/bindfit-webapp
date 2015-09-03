5ff5575ac173216503fc02512687d7e1bb34f500
Varvara Efremova <varvara@echus.co>
2015-09-03 04:40:09 +0000

    Componentised fit export button

---
+++ app/components/fit-export.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    actions: {
+        exportFit: function() {
+            var _this = this;
+
+            var request = {data: _this.get('data')};
+
+            console.log("actions.exportFit: request to send");
+            console.log(request);
+
+            // Send input data to backend for exporting
+            Ember.$.ajax({
+                url: "http://supramolecular.echus.co/bindfit/api/export",
+                type: "POST",
+                data: JSON.stringify(request),
+                contentType: "application/json; charset=utf-8",
+                dataType:    "json"
+            })
+            .done(function(data) {
+                // Set exported URL
+                _this.get('output').setProperties(data);
+            })
+            .fail(function(error) {
+                console.log("actions.exportFit: $.ajax: bindfit call failed");
+                console.log(error);
+            });
+        }, // exportFit
+
+        downloadFit: function() {
+            // Clear output on download 
+            this.get('output').reset();
+        } // downloadFit
+    } // actions
+});
