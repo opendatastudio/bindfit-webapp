f2ca8a693f8eed23562c70dbc06df769bf041d8e
Varvara Efremova <varvara@echus.co>
2015-09-21 14:14:05 +1000

    Added extra metadata fields in fit save form, ember bootstrap datepicker for dates

--- app/models/fit-meta.js
+++ app/models/fit-meta.js
@@ -1,11 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
+    author: null,
     name: null,
+    date: null,
+    timestamp: null,
+    ref: null,
+    host: null,
+    guest: null,
+    solvent: null,
+    temp: null,
     notes: null,
 
     reset: function() {
+        this.set('author', null);
         this.set('name', null);
+        this.set('date', null);
+        this.set('timestamp', null);
+        this.set('ref', null);
+        this.set('host', null);
+        this.set('guest', null);
+        this.set('solvent', null);
+        this.set('temp', null);
         this.set('notes', null);
     }
 });
