78e6d2eb9c9cbaa7972059758782f7e4ced8e0f2
Varvara Efremova <varvara@echus.co>
2015-09-23 16:55:37 +1000

    Moved chartData and chartOptions back to models for reusability between fitter and view controllers

--- app/models/fit-labels.js
+++ app/models/fit-labels.js
@@ -12,4 +12,42 @@ export default Ember.Object.extend({
     },
 
     params: null,
+
+    chartOptions: function() {
+        // Generate Highcharts options formatted fit labels
+
+        var x      = this.get("x");
+        var y      = this.get("y");
+
+        var chartOptions = {
+            chart: {
+                marginRight: 100, // For chart stacking consistency w/
+                                  // differing axis label lengths
+            },
+            title: {
+                text: "",
+            },
+            subtitle: {
+                text: "",
+            },
+            xAxis: {
+                title: {
+                    text: x.label
+                },
+                labels: {
+                    format: "{value} "+x.units
+                }
+            },
+            yAxis: { // Primary y axis
+                title: {
+                    text: y.label
+                },
+                labels: {
+                    format: "{value} "+y.units
+                }
+            }
+        };
+
+        return chartOptions;
+    }.property("x", "y"),
 });
