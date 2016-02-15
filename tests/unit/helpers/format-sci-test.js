879f36ab06a75ab6ce5a07fa9d2fd62b450d081f
Varvara Efremova <varvara@echus.co>
2016-02-15 12:22:04 +1100

    Added Matlab shortG-style scientific notation formatter helper

---
+++ tests/unit/helpers/format-sci-test.js
@@ -0,0 +1,10 @@
+import { formatSci } from '../../../helpers/format-sci';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | format sci');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = formatSci(42);
+  assert.ok(result);
+});
