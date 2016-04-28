0dd7ed1f1a8a92dbb0cb5fbc95fdbd844157d248
Varvara Efremova <varvara@echus.co>
2016-04-28 14:51:59 +1000

    Semi-working edit view with fit ID and edit key, and selection being set correctly

---
+++ app/helpers/get-fitter-selection.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default function getFitterSelection(fitter, fitList) {
+    /***
+     * Return full fitter selection details given fitter key
+     */
+
+    var match = null;
+
+    for (let i = 0; i < fitList.length; i++) {
+        if (fitList[i].key == fitter) {
+            match = fitList[i];
+            break;
+        }
+    }
+
+    return match;
+}
