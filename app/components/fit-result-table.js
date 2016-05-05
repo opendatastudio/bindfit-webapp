118d9b1302a0680f1556cad1e4a2b2acb8781500
James Wilmot <jameswilmot2000@gmail.com>
2016-05-05 18:57:12 +1000

    Results pagination: experiments in pagination

--- app/components/fit-result-table.js
+++ app/components/fit-result-table.js
@@ -1,9 +1,30 @@
 import Ember from 'ember';
 
+
+const ROWS_PER_PAGE = 10;
+
+
 export default Ember.Component.extend({
-    paramsLabelled: function() {
-        console.log("COMPONENT fit-result-table: called");
-        var labels = this.get("fitLabels.fit.params");
-        return this.get("fitResult").paramsLabelled(labels);
-    }.property("fitResult.params", "fitLabels.fit.params"),
+   
+  currentPage: 1,
+  countPages: 10,
+
+
+  paramsLabelled: function() {
+    console.log("COMPONENT fit-result-table: called");
+    var labels = this.get("fitLabels.fit.params");
+
+    var results = 
+      this.get("fitResult").paramsLabelled(labels);
+
+    console.log(results.length);
+
+    return results;
+  }.property("fitResult.params", "fitLabels.fit.params"),
+
+  actions: {
+    pageChanged(current, previous) {
+      console.log(current, previous);
+    }
+  }
 });
