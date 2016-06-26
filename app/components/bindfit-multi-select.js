2b703ede22079b10be6ac51d6f47294caa0c7794
James Wilmot <jameswilmot2000@gmail.com>
2016-06-26 14:04:17 +1000

    Data-select-ability: surgery to bring fit-table-results in line with fit-charts paging; infrastructure for selecting of fits

--- app/components/bindfit-multi-select.js
+++ app/components/bindfit-multi-select.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+/*  fits: null,*/
+  //choices: null,
+  /*selectedFits: null,*/
 });
--- app/controllers/view.js
+++ app/controllers/view.js
@@ -5,6 +5,15 @@ export default Ember.Controller.extend({
     NUMBER_ROWS_PAGE: 5,
     currentPage: 1,
     countPages: 0, // set onUploadComplete
+    
+    usePicker: false,
+
+    selectedFits: [],
+
+
+    test: Ember.observer("usePicker", function() {
+      this.debug("usePicker value: ", this.get("usePicker"));
+    }),
 
     paramsLabelled: function() {
         /***
@@ -13,7 +22,14 @@ export default Ember.Controller.extend({
         var labels = this.get("model.fitLabels.fit.params");
         return this.get("model.fitResult").paramsLabelled(labels);
     }.property("model.fitResult.fit.params", "model.fitLabels.fit.params"),
+   
     
+    selectedFitsChanged: Ember.observer('selectedFits.[]' ,function() {
+        this.debug("hi world from controller!!!");
+        var fitResult = _this.get("model.fitResult");
+
+        return fitResult;
+    }),
     
     // TODO generalise for everything, really
     pagedFitResults: function() {
@@ -62,9 +78,31 @@ export default Ember.Controller.extend({
               fitResult.qof.residuals.slice(startIndex, endIndex);
         }
 
+        if (fitResult.qof.cov) {
+            paged["qof"]["cov"]= 
+              fitResult.qof.cov.slice(startIndex, endIndex);
+        }
+        
+        if (fitResult.qof.rms) {
+            paged["qof"]["rms"]= 
+              fitResult.qof.rms.slice(startIndex, endIndex);
+        }
+
         if (fitResult.fit.y) {
             paged["fit"]["y"] = 
                 fitResult.fit.y.slice(startIndex,endIndex);
+        }
+        
+        if (fitResult.fit.coeffs[0]) {
+            paged.fit.coeffs[0] = fitResult.fit.coeffs[0].slice(startIndex, endIndex);
+        }
+        
+        if (fitResult.fit.coeffs[1]) {
+            paged.fit.coeffs[1] = fitResult.fit.coeffs[1].slice(startIndex, endIndex);
+        }
+
+        if (fitResult.fit.coeffs[2]) {
+            paged.fit.coeffs[2] = fitResult.fit.coeffs[2].slice(startIndex, endIndex);
         }
 
         return paged;
