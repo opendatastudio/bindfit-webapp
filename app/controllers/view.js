e998507d0696db378c3bf4cdc11eac9d58de6533
James Wilmot <jameswilmot2000@gmail.com>
2016-06-29 22:15:39 +1000

    View controller: removed unnecessary import

--- app/controllers/view.js
+++ app/controllers/view.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import { fitDataSlice, fitDataFilter } from '../utils/fit-data';
 import BaseController from '../mixins/base-controller';
 
 export default Ember.Controller.extend(BaseController, {
